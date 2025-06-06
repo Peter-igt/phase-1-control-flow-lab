d2db3eb12ae060718139797ad723505d05f51535
Peter Wambua <pierremumo@gmail.com>
2025-06-06 20:16:13 +0300

    Fix syntax and logic issues in fare calculation function

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function (distanceInFeet){
+  let fare;
+
+  if (distanceInFeet <=400)
+    fare = 0;
+  
+}  if (distanceInFeet <= 400) {
+  fare = 0;
+} else if (distanceInFeet > 400 && distanceInFeet < 2000) {
+  fare = (distanceInFeet - 400) * 0.02;
+} else if (distanceInFeet >= 2000 && distanceInFeet <= 2500) {
+  fare = 25;
+} else {
+  fare = 'cannot travel that far';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+return fare;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+
+function switchOnCharmFromTip(tip) {
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+}
+}   
+  
+
+
+
+
+
